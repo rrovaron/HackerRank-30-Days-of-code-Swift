7f074e6292788437bee91f40f28c617d26fd83eb
Rodrigo Rovaron <rrovaron@gmail.com>
2022-09-25 16:25:59 -0300

    Day 21: Generics

---
+++ Day 21: Generics.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct Printer<T> {
+	
+    func printArray(array: [T]) {
+        for item in array {
+            print(item)
+        }
+    }
+}
+
+
+var n = Int(readLine()!)!
+var intArray = Array(repeating: 0, count: n);
+for i in 0...n - 1 {
+	intArray[i] = Int(readLine()!)!;
+}
+
+n = Int(readLine()!)!
+var stringArray = Array(repeating: "", count: n);
+for i in 0...n - 1 {
+	stringArray[i] = readLine()!;
+}
+
+Printer<Int>().printArray(array: intArray)
+Printer<String>().printArray(array: stringArray)
